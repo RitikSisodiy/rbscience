cae80e3ddd4c26da989be8d18c7433e8442b364b
ritik sisiodiya <ritik.s10120@gmail.com>
2021-11-12 13:25:23 +0530

    remove search button

--- rbscience/wsgi.py
+++ rbscience/wsgi.py
@@ -11,10 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
-from whitenoise.django import DjangoWhiteNoisee
 from django.conf import settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rbscience.settings')
 
 application = get_wsgi_application()
 application = WhiteNoise(application, root=settings.STATIC_ROOT)
-application.add_files(root = settings.MEDIA_ROOT, prefix='/media/')
