f69b14e36241e13d932c510456fe2bffb4cba6c6
ritik sisiodiya <ritik.s10120@gmail.com>
2021-11-12 13:31:21 +0530

    fix data

--- rbscience/settings.py
+++ rbscience/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'superuser.middleware.simple_middleware'
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
