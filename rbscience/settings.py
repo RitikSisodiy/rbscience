7135453e1a548d0c1581015d65f72f943b8853f0
RitikSisodiy <ritik.s10120@gmail.com>
2021-11-28 00:55:27 +0530

    add env

--- rbscience/settings.py
+++ rbscience/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-xb6$@(*o1q#+^*fxfzs4!@of9(#x627_5m4+q(7jf!9*)w!36r
 # SECURITY WARNING: don't run with debug turned on in production!
 
 # DEBUG = False
-DEBUG = True
+import os
+if os.environ.get('enviorment') == 'production':
+    DEBUG = False
+else:
+    DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
