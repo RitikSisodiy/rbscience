75b7a160e5ec421c1ec0bee9a4a3538bdb526c14
RitikSisodiy <ritik.s10120@gmail.com>
2021-12-15 22:18:10 +0530

    fix seo

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index , name='home' ),
-    path('JOURNAL-OF-PHARMACOLOGY-AND-BIOMEDICINE', views.changenav , name='changenav' ),
-    path('rbsciencehome', views.rbsciencehome , name='rbsciencehome' ),
-    path('logindashboard', views.logindashboard, name="logindashboard"),
+    path('home/', views.changenav , name='changenav' ),
+    path('rbsciencehome/', views.rbsciencehome , name='rbsciencehome' ),
+    path('logindashboard/', views.logindashboard, name="logindashboard"),
 ]
